eae6be895cf6b8df842627a1a5264811e9d28009
Nikita Nikitins <nikita.nikitins@entrypoint.lv>
2021-04-27 20:46:13 +0300

    finished

---
+++ Raksti_1/GeometryInitializer.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Raksti_1
+{
+    public class GeometryInitializer
+    {
+        private GeometryGroup GetGeometryGroup1_Elipses()
+        {
+            GeometryGroup geometryGroup = new GeometryGroup();
+
+            for (int x = 20, y = 10; y >= 1; y -= 3, x -= 5)
+            {
+                geometryGroup.Children.Add(new EllipseGeometry(new Point(50, 50), x, y));
+                geometryGroup.Children.Add(new EllipseGeometry(new Point(50, 50), y, x));
+            }
+
+            return geometryGroup;
+        }
+
+        private GeometryGroup GetGeometryGroup2()
+        {
+            GeometryGroup geometryGroup = new GeometryGroup();
+
+            geometryGroup.Children.Add(new EllipseGeometry(new Point(110, 60), 50, 50));
+            geometryGroup.Children.Add(new EllipseGeometry(new Point(80, 110), 50, 50));
+            geometryGroup.Children.Add(new EllipseGeometry(new Point(140, 110), 50, 50));
+
+            return geometryGroup;
+        }
+
+        private GeometryGroup GetGeometryGroup3()
+        {
+            GeometryGroup geometryGroup = new GeometryGroup();
+
+            for (int x = 50; x >= 5; x -= 5)
+                geometryGroup.Children.Add(new EllipseGeometry(new Point(110, 60), x, x));
+
+            return geometryGroup;
+        }
+
+        private PathGeometry DogsBone()
+        {
+            var pathGeometry = new PathGeometry();
+            var pathSegments = new List<PathSegment>();
+            var pathFigure = new PathFigure();
+            pathFigure.StartPoint = new Point(70, 60);
+            pathFigure.IsClosed = true;
+
+            pathSegments.Add(new ArcSegment(new Point(40, 80), new Size(20, 20), 0, true, new SweepDirection(), true));
+            pathSegments.Add(new ArcSegment(new Point(70, 100), new Size(20, 20), 0, true, new SweepDirection(), true));
+            pathSegments.Add(new LineSegment(new Point(160, 100), true));
+            pathSegments.Add(new ArcSegment(new Point(190, 80), new Size(20, 20), 0, true, new SweepDirection(), true));
+            pathSegments.Add(new ArcSegment(new Point(160, 60), new Size(20, 20), 0, true, new SweepDirection(), true));
+
+            foreach (var pathSegment in pathSegments)
+                pathFigure.Segments.Add(pathSegment);
+
+            pathGeometry.Figures.Add(pathFigure);
+
+            return pathGeometry;
+        }
+
+        private PathGeometry GetshurikenPathGeometry()
+        {
+            var pathGeometry = new PathGeometry();
+            var polyLineSegments = new List<PolyLineSegment>();
+            var pathFigure = new PathFigure();
+            pathFigure.StartPoint = new Point(20, 80);
+            pathFigure.IsClosed = true;
+
+            var points = new List<Point>();
+            points.Add(new Point(60, 60));
+            points.Add(new Point(90, 75));
+            points.Add(new Point(75, 45));
+            points.Add(new Point(95, 5));
+            points.Add(new Point(115, 45));
+            points.Add(new Point(100, 75));
+            points.Add(new Point(130, 60));
+            points.Add(new Point(170, 80));
+            points.Add(new Point(130, 100));
+            points.Add(new Point(100, 85));
+            points.Add(new Point(115, 115));
+            points.Add(new Point(95, 155));
+            points.Add(new Point(75, 115));
+            points.Add(new Point(90, 85));
+            points.Add(new Point(60, 100));
+
+            polyLineSegments.Add(new PolyLineSegment(points, true));
+
+            pathFigure.Segments.Add(polyLineSegments.First());
+
+            pathGeometry.Figures.Add(pathFigure);
+
+            return pathGeometry;
+        }
+
+        public Geometry GetSelectedGeometry(int _selectedRadioButtonTag)
+        {
+            switch (_selectedRadioButtonTag)
+            {
+                case 1:
+                    return GetGeometryGroup1_Elipses();
+
+                case 2:
+                    return GetGeometryGroup2();
+
+                case 3:
+                    return GetGeometryGroup3();
+
+                case 4:
+                    return DogsBone();
+
+                case 5:
+                    return GetshurikenPathGeometry();
+                default:
+                    return GetGeometryGroup1_Elipses();
+            }
+        }
+    }
+}
--- Raksti_1/MainWindow.xaml.cs
+++ Raksti_1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,14 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using KeyEventArgs = System.Windows.Forms.KeyEventArgs;
+using RadioButton = System.Windows.Controls.RadioButton;
 
 namespace Raksti_1
 {
@@ -20,9 +24,129 @@ namespace Raksti_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int _selectedRadioButtonTag { get; set; }
+        private GeometryInitializer geometryInitializer { get; set; }
+        private Pen _pen { get; set; }
+        private Brush _brush { get; set; }
+        private Brush _canvasBackground { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            geometryInitializer = new GeometryInitializer();
+
+            _selectedRadioButtonTag = 1;
+
+            _pen = new Pen(Brushes.Red, 1);
+
+            _brush = Brushes.Green;
+
+            _canvasBackground = Brushes.White;
+
+            PenColorBtn.BorderBrush = _pen.Brush;
+            BrushColorBtn.BorderBrush = _brush;
+            BackgroundColorBtn.BorderBrush = _canvasBackground;
+
+            SetUpRadioButtonImages();
+        }
+
+        private void onPaint(object sender, RoutedEventArgs e)
+        {
+            var geometryGroup = geometryInitializer.GetSelectedGeometry(_selectedRadioButtonTag);
+            int scaleFactor = 1;
+
+            try
+            {
+                scaleFactor= int.Parse(scaleFactorField.Text);
+            }
+            catch
+            {
+            }
+
+            fillCanvas(drawingCanvasField, geometryGroup, scaleFactor);
+
+            SetUpRadioButtonImages();
+        }
+
+        private void fillCanvas(Canvas canvas, Geometry geometry, int scaleFactor = 1)
+        {
+
+            GeometryDrawing geometryDrawing = new GeometryDrawing();
+            geometryDrawing.Geometry = geometry;
+            geometryDrawing.Brush = _brush;
+            geometryDrawing.Pen = _pen;
+
+            DrawingBrush drawingBrush = new DrawingBrush();
+            drawingBrush.Drawing = geometryDrawing;
+
+            drawingBrush.Viewport = new Rect(0, 0, 1.0 / scaleFactor, 1.0 / scaleFactor);
+            drawingBrush.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
+            drawingBrush.TileMode = TileMode.Tile;
+
+            canvas.Background = drawingBrush;
+
+            canvasBackground.Background = _canvasBackground;
+        }
+
+        private void SetUpRadioButtonImages()
+        {
+            fillCanvas(firstImg, geometryInitializer.GetSelectedGeometry(1));
+            fillCanvas(secondImg, geometryInitializer.GetSelectedGeometry(2));
+            fillCanvas(thirdImg, geometryInitializer.GetSelectedGeometry(3));
+            fillCanvas(fourthImg, geometryInitializer.GetSelectedGeometry(4));
+            fillCanvas(fifthImg, geometryInitializer.GetSelectedGeometry(5));
+        }
+
+        private void graphicsSelected(object sender, RoutedEventArgs e)
+        {
+            _selectedRadioButtonTag = int.Parse((sender as RadioButton).Tag.ToString());
+
+            onPaint(sender, e);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _pen = new Pen(new SolidColorBrush(GetColorFromDialog()), 1);
+
+            PenColorBtn.BorderBrush = _pen.Brush;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _brush = new SolidColorBrush(GetColorFromDialog());
+
+            BrushColorBtn.BorderBrush = _brush;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            _canvasBackground = new SolidColorBrush(GetColorFromDialog());
+
+            BackgroundColorBtn.BorderBrush = _canvasBackground;
+        }
+
+        private Color GetColorFromDialog()
+        {
+            var c = new Color();
+
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                c = Color.FromArgb(colorDialog.Color.A, colorDialog.Color.R, colorDialog.Color.G, colorDialog.Color.B);
+
+            return c;
+        }
+
+        private void F1_Key_Down(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+            {
+                System.Windows.MessageBox.Show(@"
+                              Author: Nikita Nikitins
+                              E-mail: nikitinsn6@gmail.com
+                              Github: https://github.com/NikitaNikitins
+                              Project name: Grafikas Attēlošana ar WPF");
+            }
         }
     }
 }
